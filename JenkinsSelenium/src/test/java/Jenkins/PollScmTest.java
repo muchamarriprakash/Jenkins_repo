131fc5a5fe194504f9c0822c6a4d0d2f4819b40b
hp <hp@DESKTOP-B0Q4AIF>
2024-08-16 11:48:35 +0530

    first commit

---
+++ JenkinsSelenium/src/test/java/Jenkins/PollScmTest.java
@@ -0,0 +1,14 @@
+package Jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScmTest 
+{
+	@Test
+	public void PollScm()
+	{
+		Reporter.log("poll scm test add execution",true);
+	}
+
+}
